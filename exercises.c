ecd8bc7e5dbf09ea61f04d383462799dd01a2a6a
Your Name <you@example.com>
2024-03-23 21:12:11 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -118,6 +118,7 @@ int parentesisBalanceados(char *cadena) {
   char letra, *primLetra;
   for (unsigned short i = 0; i < cant; i++) {
     letra = cadena[i];
+    printf("%c",letra);
     if (letra == '(' || letra == '[' || letra == '{')
       push(pila, &letra);
     else if (letra == ')' || letra == ']' || letra == '}') {
